3d7a628cef0f9dcc2fb7f5c6d03d03b43d9f5773
Zelyna Farrell <zsillas@yahoo.com>
2023-11-11 13:40:27 -0800

    Initial Commit

---
+++ WalletAppAnimation/WalletAppAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletAppAnimationApp.swift
+//  WalletAppAnimation
+//
+//  Created by Zelyna Sillas on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WalletAppAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
